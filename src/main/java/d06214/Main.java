0c909eb61520d37c81d3d8b5b5d6524fb8408340
ksjzheld123 <rlatjswnzheld970616@gmail.com>
2024-06-21 16:48:51 +0900

    SDT0621

---
+++ src/main/java/d06214/Main.java
@@ -0,0 +1,20 @@
+package d06214;
+
+
+
+//        |\_/|
+//        |q p|   /}
+//        ( 0 )"""\
+//        |"^"`    |
+//        ||_/=\\__|
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
